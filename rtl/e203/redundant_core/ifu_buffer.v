e608b1238aa9c15f8e7bc3fc2958e586e9ca80bc
FxPiGaAo <xhc0@mail.ustc.edu.cn>
2019-02-20 01:51:59 -0600

    complete the ifu_buffer module

--- rtl/e203/redundant_core/ifu_buffer.v
+++ rtl/e203/redundant_core/ifu_buffer.v
@@ -105,6 +105,7 @@ module ifu_buffer(
   //input  dec2ifu_remu  ,
 
   input  clk,
+  input lden,
   //input  rst_n
   
   
@@ -112,35 +113,17 @@ module ifu_buffer(
   input in_ifu_active,
 
   `ifdef E203_HAS_ITCM //{
-
-
-  //////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////
-  // Bus Interface to ITCM, internal protocol called ICB (Internal Chip Bus)
-  //    * Bus cmd channel
   input in_ifu2itcm_icb_cmd_valid, // Handshake valid
   input [`E203_ITCM_ADDR_WIDTH-1:0]   in_ifu2itcm_icb_cmd_addr, // Bus transaction start addr 
-
-  //    * Bus RSP channel
   input in_ifu2itcm_icb_rsp_ready, // Response ready
   `endif//}
 
   `ifdef E203_HAS_MEM_ITF //{
-  //////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////
-  // Bus Interface to System Memory, internal protocol called ICB (Internal Chip Bus)
-  //    * Bus cmd channel
   input in_ifu2biu_icb_cmd_valid, // Handshake valid
   input [`E203_ADDR_SIZE-1:0]   in_ifu2biu_icb_cmd_addr, // Bus transaction start addr 
-
-  //    * Bus RSP channel
   input in_ifu2biu_icb_rsp_ready, // Response ready
 
   `endif//}
-
-  //////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////
-  // The IR stage to EXU interface
   input [`E203_INSTR_SIZE-1:0] in_ifu_o_ir,// The instruction register
   input [`E203_PC_SIZE-1:0] in_ifu_o_pc,   // The PC register along with
   input in_ifu_o_pc_vld,
@@ -151,7 +134,6 @@ module ifu_buffer(
   input in_ifu_o_prdt_taken,               // The Bxx is predicted as taken
   input in_ifu_o_muldiv_b2b,               
   input in_ifu_o_valid, // Handshake signals with EXU stage
-
   input  in_pipe_flush_ack,
   `ifdef E203_TIMING_BOOST//}
   `endif//}
@@ -159,5 +141,56 @@ module ifu_buffer(
   input in_ifu_halt_ack
   );
   
+  
+   sirv_gnrl_dffl #(`E203_PC_SIZE)  inspect_pc_dffl (lden, in_inspect_pc, inspect_pc, clk);
+   //input[`E203_PC_SIZE-1:0] in_inspect_pc,
+   sirv_gnrl_dffl #(1)  ifu_active_dffl (lden, in_ifu_active, ifu_active, clk);
+   //input in_ifu_active,
+
+  `ifdef E203_HAS_ITCM //{
+  //input in_ifu2itcm_icb_cmd_valid, // Handshake valid
+  sirv_gnrl_dffl #(1)  ifu2itcm_icb_cmd_valid_dffl (lden, in_ifu2itcm_icb_cmd_valid, ifu2itcm_icb_cmd_valid, clk);
+  //input [`E203_ITCM_ADDR_WIDTH-1:0]   in_ifu2itcm_icb_cmd_addr, // Bus transaction start addr 
+  sirv_gnrl_dffl #(`E203_ITCM_ADDR_WIDTH)  ifu2itcm_icb_cmd_addr_dffl (lden, in_ifu2itcm_icb_cmd_addr, ifu2itcm_icb_cmd_addr, clk);
+  //input in_ifu2itcm_icb_rsp_ready, // Response ready
+  sirv_gnrl_dffl #(1)  ifu2itcm_icb_rsp_ready_dffl (lden, in_ifu2itcm_icb_rsp_ready, ifu2itcm_icb_rsp_ready, clk);
+  `endif//}
+
+  `ifdef E203_HAS_MEM_ITF //{
+  //input in_ifu2biu_icb_cmd_valid, // Handshake valid
+  sirv_gnrl_dffl #(1)  ifu2biu_icb_cmd_valid_dffl (lden, in_ifu2biu_icb_cmd_valid, ifu2biu_icb_cmd_valid, clk);
+  //input [`E203_ADDR_SIZE-1:0]   in_ifu2biu_icb_cmd_addr, // Bus transaction start addr
+  sirv_gnrl_dffl #(`E203_ADDR_SIZE)  ifu2biu_icb_cmd_addr_dffl (lden, in_ifu2biu_icb_cmd_addr, ifu2biu_icb_cmd_addr, clk); 
+  //input in_ifu2biu_icb_rsp_ready, // Response ready
+  sirv_gnrl_dffl #(1)  ifu2biu_icb_rsp_ready_dffl (lden, in_ifu2biu_icb_rsp_ready, ifu2biu_icb_rsp_ready, clk);
+
+  `endif//}
+  //input [`E203_INSTR_SIZE-1:0] in_ifu_o_ir,// The instruction register
+  sirv_gnrl_dffl #(`E203_INSTR_SIZE)  ifu_o_ir_dffl (lden, in_ifu_o_ir, ifu_o_ir, clk);
+  //input [`E203_PC_SIZE-1:0] in_ifu_o_pc,   // The PC register along with
+  sirv_gnrl_dffl #(`E203_PC_SIZE)  ifu_o_pc_dffl (lden, in_ifu_o_pc, ifu_o_pc, clk);
+  //input in_ifu_o_pc_vld,
+  sirv_gnrl_dffl #(1)  ifu_o_pc_vld_dffl (lden, in_ifu_o_pc_vld, ifu_o_pc_vld, clk);
+  //input in_ifu_o_misalgn,                  // The fetch misalign
+  sirv_gnrl_dffl #(1)  ifu_o_misalgn_dffl (lden, in_ifu_o_misalgn, ifu_o_misalgn, clk); 
+  //input in_ifu_o_buserr,                   // The fetch bus error
+  sirv_gnrl_dffl #(1)  ifu_o_buserr_dffl (lden, in_ifu_o_buserr, ifu_o_buserr, clk);
+  //input [`E203_RFIDX_WIDTH-1:0] in_ifu_o_rs1idx,
+  sirv_gnrl_dffl #(`E203_RFIDX_WIDTH)  ifu_o_rs1idx_dffl (lden, in_ifu_o_rs1idx, ifu_o_rs1idx, clk);
+  //input [`E203_RFIDX_WIDTH-1:0] in_ifu_o_rs2idx,
+  sirv_gnrl_dffl #(`E203_RFIDX_WIDTH)  ifu_o_rs2idx_dffl (lden, in_ifu_o_rs2idx, ifu_o_rs2idx, clk);
+  //input in_ifu_o_prdt_taken,               // The Bxx is predicted as taken
+  sirv_gnrl_dffl #(1)  ifu_o_prdt_taken_dffl (lden, in_ifu_o_prdt_taken, ifu_o_prdt_taken, clk);
+  //input in_ifu_o_muldiv_b2b,               
+  sirv_gnrl_dffl #(1)  ifu_o_muldiv_b2b_dffl (lden, in_ifu_o_muldiv_b2b, ifu_o_muldiv_b2b, clk);
+  //input in_ifu_o_valid, // Handshake signals with EXU stage
+  sirv_gnrl_dffl #(1)  ifu_o_valid_dffl (lden, in_ifu_o_valid, ifu_o_valid, clk);
+  //input  in_pipe_flush_ack,
+  sirv_gnrl_dffl #(1)  pipe_flush_ack_dffl (lden, in_pipe_flush_ack, pipe_flush_ack, clk);
+  `ifdef E203_TIMING_BOOST//}
+  `endif//}
+
+  //input in_ifu_halt_ack
+  sirv_gnrl_dffl #(1)  ifu_halt_ack_dffl (lden, in_ifu_halt_ack, ifu_halt_ack, clk);
 endmodule
 
